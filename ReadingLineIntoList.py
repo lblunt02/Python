a185eec78da0c27fbcd062b7ab4853f1552e3c7d
Kyle Bradshaw <kylebradshaw@Kyles-Air.attlocal.net>
2024-02-14 12:34:35 -0500

    functions

---
+++ ReadingLineIntoList.py
@@ -0,0 +1,19 @@
+with open('example.txt', 'r') as file:
+    lines = file.readlines()
+    print(lines)
+
+    
+def our_math(a, b):
+    return a + b
+
+our_math(5, 2)
+print()
+
+def second_function():
+    a = 1
+    b = 2
+    
+    return a + b
+
+second_function()
+
