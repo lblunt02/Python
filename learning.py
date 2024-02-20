3f384b9ddb6a8253d35a3899d70c92e607838cf8
Lisa <lblunt02@gmail.com>
2024-02-20 18:18:09 -0500

    learning

--- learning.py
+++ learning.py
@@ -1,3 +1,5 @@
-message = 'One of Python\'s strengths is its "diverse community."'
-print(message)
+# message = 'One of Python\'s strengths is its "diverse community."'
+# print(message)
 # Lisa
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1].title())
