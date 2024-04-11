57d95ec50540553dca3eac2aa78182eaa12ce04c
kabradshaw1 <itllbok82@gmail.com>
2024-04-11 14:57:07 -0400

    python chapter 2 practice

--- learning.py
+++ learning.py
@@ -1,2 +1,12 @@
-message = "One of Python's strengths is its \"diverse community.\""
-print(message)
+name = "ada lovelace"
+print(name.title())
+
+
+person_name = "John Doe"
+
+print(person_name.lower())
+
+print(person_name.upper())
+
+print(person_name.title())
+
---
+++ learning2.py
@@ -0,0 +1,8 @@
+bisycle = ['trek', 'cannondale', 'redline', 'specialized', 'huffy', 'lisa']
+
+x = 0
+while x < 5:
+    message = f"The bike is {bisycle[x].title()}"
+    print(message)
+    x += 1
+
